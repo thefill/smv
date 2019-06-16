b2ec35756e193af3463a4e73ec1123f7f975ade0
Filip Dabrowski <fifofil@gmail.com>
2019-06-17 00:36:27 +0100

    Initial implementation

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,2 +1,28 @@
 export * from './utils';
 export * from './smv';
+
+// TODO: remove below
+import {SMV} from './smv';
+
+const defaultDependencies = {
+    '@scoped/package': '1.2.3'
+    // 'hyphened-package': '1.2.3',
+    // 'default-package': '1.2.3',
+    // 'patch-package': '^1.2.3',
+    // 'minor-package': '~1.2.3'
+};
+
+const conflictingDependencies = {
+    '@scoped/package': '2.2.3'
+    // 'hyphened-package': '2.2.3',
+    // 'default-package': '1.2.3',
+    // 'patch-package': '^1.5.3',
+    // 'minor-package': '~2.2.3'
+};
+
+const smv = new SMV();
+
+const resolution = smv.merge({defaultDependencies, conflictingDependencies});
+
+// tslint:disable-next-line
+console.log(resolution);
